8ecdc5bf3428fa362eeb20f60b56a7a51907f610
megazbik <51079261+megazbik@users.noreply.github.com>
2019-06-30 19:18:27 +0200

    Add files via upload

---
+++ rep.js
@@ -0,0 +1,8 @@
+$('.Hide').click(function() {
+    $('#target').hide(500);
+    $('.Show').show(0);
+    $('.Hide').hide(0);
+});
+$('.toggle').click(function() {
+    $('#target').toggle('slow');
+});
